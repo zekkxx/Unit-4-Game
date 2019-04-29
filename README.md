b9bda560d66b7f67d0f4c6fe99d09c8c37ed3e54
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2019-04-29 17:21:10 -0500

    Committing fully functional game, forget to make smaller commuts along the way.

--- README.md
+++ README.md
@@ -23,3 +23,6 @@ At Game Start - Player chooses a fighter, all other fighters become enemy combat
 Crystal Collector game is all based on randomization and experimentation. If the buttons are all closer to 12 and the value is closer to 19 the game will be nearly impossible. As such, it might be an interesting challenge to make sure that there IS a solution to EVERY challenge that comes along.
 
 Star Wars RPG game will be more of a balancing act. With health being a limited resource the important point is to find the person with the lowest Counter-Attack, and striking at them first. This is going to take more time in my opinion to make suitable fighters and balance them than to actually program.
+
+#Choice
+I went with the Star Wars RPG game.
--- assets/javascript/swRPG.js
+++ assets/javascript/swRPG.js
@@ -1,58 +1,128 @@
-//characterChoice = {Health, Attack, Counter}
-var type1;
-var type2;
-var type3;
-var round;
+var myGame;
 
-function assign(){
-    type1 = [50, 1, 4];
-    type2 = [25, 3, 4];
-    type3 = [15, 5, 10];
-    round = 0;
-    console.log("New Round Started");
-}
+class Game{
+    constructor(){
+        this.characterArray = [["C3PO", 50, 1, 4], ["R2D2", 25, 3, 4], ["BB8", 15, 5, 10]];;
+        this.round = 0;
+        this.victories = 0;
+        this.characterSelected = false;
+        this.targetSelected = false;
+        this.myCharacter;
+        this.target;
+        this.setUpCharacterSelect();
+    }
 
-function battle(player, enemy){
-    var battleOver = false;
-    var result=""
-    if(player[0]==0){
-        console.log("Player already dead");
-    } else {
-        while(battleOver!=true){
-            round++;
-            enemy[0]=enemy[0]-player[1]*round;
-            if(enemy[0]<=0){
-                enemy[0]=0;
-                result=" Success";
-                battleOver=true;
-            } else {
-                player[0]=player[0]-enemy[2];
-                if(player[0]<=0){
-                    player[0]=0;
-                    result=" Failure";
-                    battleOver=true;
-                }
-            }
-            console.log('('+round+')'+enemy[0]+'/'+player[0]+result);
+    setUpCharacterSelect(){ //A function to return the index.html page to starting conditions
+        $(".header").html("<h2>Character Select</h2>"); //Resets Header page
+        var charImgString = "";
+        for(var i = 0; i<this.characterArray.length; i++){ //Sets up an img using the following criteria
+            charImgString+='<img class="charImg" id="' //img of class charImg
+                +this.characterArray[i][0] //with an id of the first element in the character aray
+                +'" src="assets/images/' //with img from source
+                +this.characterArray[i][0]
+                +'.jpg" value="'+i+'" />' //With a value of the index in array
+        }
+        $(".characters").html(charImgString+'<div id="report"></div>');
+        $(".target").html("");
+    }
+
+    imgClickDefiner(input){
+        if(this.characterSelected!=true){
+            this.assignCharacterToPlayer(input);
+        } else if(this.targetSelected!=true){
+            this.assignCharacterToTarget(input);
+            $("#report").text("");
+        } else {
+            alert("Target already chosen.")
+        }
+        //console.log(input.attributes.value.value);
+        //console.log(input.id);
+    }
+
+    assignCharacterToPlayer(input){
+        $("#"+input.id).remove();
+        this.myCharacter = this.characterArray[input.attributes.value.value];
+        this.characterSelected=true;
+        this.updatePlayer();
+    }
+
+    updatePlayer(){
+        var playerHTML = '<img src="assets/images/'+this.myCharacter[0]+'.jpg" />'
+        playerHTML += '<div><h1>Player:';
+        playerHTML += '<h1>Health: '+this.myCharacter[1]+'</h1></div>';
+        $(".header").html(playerHTML);
+    }
+
+    assignCharacterToTarget(input){
+        $("#"+input.id).remove();
+        this.target = this.characterArray[input.attributes.value.value];
+        this.targetSelected=true;
+        this.updateTarget();
+    }
+
+    updateTarget(){
+        if(this.targetSelected==true){
+            var targetHTML = '<img src="assets/images/'+this.target[0]+'.jpg" />'
+            targetHTML += '<div style="text-align: center"><h1>Health: '+this.target[1]+'</h1>';
+            targetHTML += '<button onclick="battle()">Attack!</button></div>'
+            $(".target").html(targetHTML);
+        } else {
+            $(".target").html("");
+        }
+    }
+
+    fight(){
+        this.round++;
+        this.target[1] -= this.round * this.myCharacter[2];
+        if(this.target[1]<=0){
+            this.target="";
+            this.targetSelected=false;
+            this.victories++;
+            this.checkForWin();
+        } else {
+            this.myCharacter[1] -= this.target[3];
+            this.checkForLoss();
+        }
+    }
+
+    fullUpdate(){
+        this.updatePlayer();
+        this.updateTarget();
+    }
+
+    checkForWin(){
+        if(this.victories>=this.characterArray.length-1){
+            alert("Victory! You are the greatest droid!");
+            this.myCharacter="";
+            startNewGame();
+        } else {
+            $("#report").text("Select Your Next Opponent");
+            this.fullUpdate();
+        }
+    }
+
+    checkForLoss(){
+        if(this.myCharacter[1]<=0){
+            alert("You have been defeated.")
+            startNewGame();
+        } else {
+            this.fullUpdate();
+            $("#report").text("You dealt "+this.round*this.myCharacter[2]+" damage! And you received "+this.target[3]+" damage!")
         }
     }
 }
 
-assign();
-battle(type1, type2);
-battle(type1, type3);
-assign();
-battle(type1, type3);
-battle(type1, type2);
-assign();
-battle(type2, type1);
-battle(type2, type3);
-assign();
-battle(type2, type3);
-battle(type2, type1);
-assign();
-battle(type3, type2);
-battle(type3, type1);
-assign();
-battle(type3, type1);
-battle(type3, type2);
+$(function(){
+    startNewGame();
+});
+
+function startNewGame(){
+    myGame = new Game;
+    $(".charImg").on("click", function(){
+        myGame.imgClickDefiner(event.target);
+    })
+}
+
+function battle(){
+    myGame.fight();
+}
