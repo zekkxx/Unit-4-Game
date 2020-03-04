eab7b65913c35f30c7c66fd7720ff90cf0c1faf1
Kieran 'zekkxx' Anthony <zekkxx@gmail.com>
2020-03-04 17:16:28 -0600

    Refactored the victories count out of the project

--- assets/javascript/swRPG.js
+++ assets/javascript/swRPG.js
@@ -3,42 +3,15 @@ var myGame;
 class Game{
     constructor(){
         this.characterArray = [["C3PO", 50, 1, 4], ["R2D2", 25, 3, 4], ["BB8", 15, 5, 10]];
-        this.victories = 0;
         this.myCharacter;
         this.target;
         this.updateScreen();
     }
 
-    updateScreen(){ //A function to return the index.html page to starting conditions
+    updateScreen(){
         this.updatePlayerHTML();
-        var charImgString = "";
-        for(var i = 0; i<this.characterArray.length; i++){ //Sets up an img using the following criteria
-            charImgString+='<img class="charImg" id="' //img of class charImg
-                +this.characterArray[i][0] //with an id of the first element in the character aray
-                +'" src="assets/images/' //with img from source
-                +this.characterArray[i][0]
-                +'.jpg" value="'+i+'" />' //With a value of the index in array
-        }
-        $(".field").html(charImgString+'<div id="report"></div>');
+        this.updateFieldHTML();
         this.updateTargetHTML();
-    }
-
-    imgClickDefiner(input){
-        if(!this.myCharacter){
-            this.assignCharacterToPlayer(input);
-        } else if(!this.target){
-            this.assignCharacterToTarget(input);
-            $("#report").text("");
-        } else {
-            alert("You have already chosen your opponent.")
-        }
-    }
-
-    assignCharacterToPlayer(input){
-        $("#"+input.id).remove();
-        this.myCharacter = this.characterArray[input.attributes.value.value];
-        this.myCharacter[3] = 0;
-        this.updatePlayerHTML();
     }
 
     updatePlayerHTML(){
@@ -52,10 +25,19 @@ class Game{
         }
     }
 
-    assignCharacterToTarget(input){
-        $("#"+input.id).remove();
-        this.target = this.characterArray[input.attributes.value.value];
-        this.updateTargetHTML();
+    updateFieldHTML(){
+        if(this.target){
+            $(".field").html('<div id="report">Prepare for battle!</div>');
+        } else {
+            var charImgString = "";
+            for(var i = 0; i<this.characterArray.length; i++){ //Sets up an img using the following criteria
+                charImgString+='<img onclick="myGame.imgClickDefiner(event.target)" class="charImg"' //img of class charImg
+                    +'id="'+this.characterArray[i][0]+`"` //with an id of the first element in the character aray
+                    +'src="assets/images/'+this.characterArray[i][0]+`.jpg"` //with img from source
+                    +'value="'+i+'" />' //With a value of the index in array
+            }
+            $(".field").html(charImgString);
+        }
     }
 
     updateTargetHTML(){
@@ -69,12 +51,36 @@ class Game{
         }
     }
 
+    imgClickDefiner(input){
+        if(!this.myCharacter){
+            this.assignCharacterToPlayer(input);
+            this.characterArray.splice(input.id, 1);
+        } else if(!this.target){
+            this.assignCharacterToTarget(input);
+            this.characterArray.splice(input.id, 1);
+            $("#report").text("");
+        } else {
+            alert("You have already chosen your opponent.")
+        }
+        this.updateScreen();
+    }
+
+    assignCharacterToPlayer(input){
+        $("#"+input.id).remove();
+        this.myCharacter = this.characterArray[input.attributes.value.value];
+        this.myCharacter[3] = 0;
+    }
+
+    assignCharacterToTarget(input){
+        $("#"+input.id).remove();
+        this.target = this.characterArray[input.attributes.value.value];
+    }
+
     fight(){
         this.myCharacter[3] += this.myCharacter[2];
         this.target[1] -= this.myCharacter[3];
         if(this.target[1]<=0){
             this.target="";
-            this.victories++;
             this.checkForWin();
         } else {
             this.myCharacter[1] -= this.target[3];
@@ -82,19 +88,14 @@ class Game{
         }
     }
 
-    fullUpdate(){
-        this.updatePlayerHTML();
-        this.updateTargetHTML();
-    }
-
     checkForWin(){
-        if(this.victories>=this.characterArray.length-1){
+        if(0 >= this.characterArray.length){
             alert("Victory! You are the greatest droid!");
             this.myCharacter="";
             startNewGame();
         } else {
             $("#report").text("Select Your Next Opponent");
-            this.fullUpdate();
+            this.updateScreen();
         }
     }
 
@@ -103,7 +104,7 @@ class Game{
             alert("You have been defeated.")
             startNewGame();
         } else {
-            this.fullUpdate();
+            this.updateScreen();
             $("#report").text("You dealt "+this.myCharacter[3]+" damage! And you received "+this.target[3]+" damage!")
         }
     }
@@ -115,7 +116,4 @@ $(function(){
 
 function startNewGame(){
     myGame = new Game;
-    $(".charImg").on("click", function(){
-        myGame.imgClickDefiner(event.target);
-    })
 }
